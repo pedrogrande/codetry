b02352185d8b1452213f9c6bc9563cbbf6ead3fc
David Evans <davidjsevans@gmail.com>
2013-05-08 16:44:10 +1000

    update header, footer links, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Codetry::Application.routes.draw do
-  
-  get "privacy_policy/index"
 
-  get "faq/index"
+  get "contributors/index", :id => "contributors"
+  match "contributors" => "contributors#index"
 
-  get "press/index"
+  get "contact/index", :id => "contact"
+  match "contact" => "contact#index"
 
-  get "contact/index"
+  get "usage_policy/index", :id => "usage_policy"
+  match "usage_policy" => "usage_policy#index"
 
-  get "contributors/index"
+  get "press/index", :id => "press"
+  match "press" => "press#index"
 
-  get "usage_policy/index"
+  get "faq/index", :id => "faq"
+  match "faq" => "faq#index"
+
+  get "privacy_policy/index", :id => "privacy_policy"
+  match "privacy_policy" => "privacy_policy#index"
 
   authenticated :user do
     root :to => 'home#index'
