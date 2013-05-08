a1a9f1adb1480e942bb8f2c7a474b9d2f7dc2d1a
Jurgens Smit <contact@jurgenssmit.com>
2013-05-08 16:22:50 +1000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Codetry::Application.routes.draw do
   
-  get "privacy_policy/index"
+  get "privacy/index", :id => "privacy_policy"
+ match "privacy_policy" => "privacy_policy#index"
 
-  get "faq/index"
+ get "faq/index", :id => "faq"
+ match "faq" => "faq#index"
 
-  get "press/index"
+ get "press/index", :id => "press"
+ match "press" => "press#index"
 
-  get "contact/index"
+ get "contributors/index", :id => "contributors"
+ match "contributors" => "contributors#index"
 
-  get "contributors/index"
+ get "usage_policy/index", :id => "usage_policy"
+ match "usage_policy" => "usage_policy#index"
 
-  get "usage_policy/index"
+match 'contact' => 'contact#new', :as => 'contact', :via => :get
+match 'contact' => 'contact#create', :as => 'contact', :via => :post
 
   authenticated :user do
     root :to => 'home#index'
