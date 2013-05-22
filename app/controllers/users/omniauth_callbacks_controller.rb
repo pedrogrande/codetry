e122d05e8c4221313510c80eb36f0e48d08e9023
pedrogrande <peter.argent@gmail.com>
2013-05-22 16:34:22 +1000

    fix fa2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -30,7 +30,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #Rails.logger.info("auth is **************** #{auth.to_yaml}")
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind "Twitter"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.twitter_uid"] = request.env["omniauth.auth"]
